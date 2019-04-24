sounddrill31/AOSP_Calendar

AOSP/Calendar -Added TEST_MAPPING file and "LOCAL_COMPATIBILITY_SUITE := general-tests" to enable presubmit tests for Calendar.
Bug: 127848297

Test: manual - Ran the following CTS tests on Pixel phone. Tested the calendar UI.

$ make -j 40
$ make CalendarTests -j

$ adb install -r out/target/product/marlin/system/app/Calendar/Calendar.apk
$ adb install -r out/target/product/marlin/testcases/CalendarTests/CalendarTests.apk

$ adb shell am instrument -w com.android.calendar.tests

$ ./development/testrunner/runtest.py --path cts/tests/tests/provider/src/android/provider/cts/CalendarTest.java

atest
-----
$ cd packages/apps/Calendar
$ atest

acloud test
-----------
$ lunch aosp_cf_x86_phone-userdebug

$ make -j 40
$ make CalendarTests -j

$ acloud reconnect
$ adb devices
  127.0.0.1:52761	device

$ cd packages/apps/Calendar
$ atest CalendarTests -- --abi x86 --serial 127.0.0.1:52761

Change-Id: I2347ab83ddaa30d4e069419a4d9196ce913fe533

Android.mk
@@ -37,6 +37,8 @@ LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
 
 LOCAL_PACKAGE_NAME := Calendar
 
+LOCAL_COMPATBILITY_SUITE := general-tests
+
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags \
                              ../../../frameworks/opt/datetimepicker/proguard.flags
 

aosp-mirror/platform_packages_apps_calendar

Android.mk
@@ -37,6 +37,8 @@ LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
 
 LOCAL_PACKAGE_NAME := Calendar
 
+LOCAL_COMPATIBILITY_SUITE := general-tests
+
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags \
                              ../../../frameworks/opt/datetimepicker/proguard.flags
 