c73e51ead8efb79e53f19f8150242e12043044c4
Mahi Kolla <mahikolla@google.com>
2021-06-08 22:14:27 +0000

    AOSP/Calendar - Add Kotlin code for UpgradeReceiver.kt Also uploaded corresponding Android.bp file
    Test: manual - ran build and unit tests
    
    $ source build/envsetup.sh
    $ lunch aosp_bonito-userdebug
    $ make Calendar
    $ adb install -r -d -t out/target/product/bonito/product/app/Calendar/Calendar.apk
    
    $ make CalendarTests -j
    $ adb install -r -d -t out/target/product/bonito/testcases/CalendarTests/arm64/CalendarTests.apk
    $ adb shell am instrument -w com.android.calendar.tests
    
    com.android.calendar.FormatDateRangeTest:.
    com.android.calendar.UtilsTests:..................
    com.android.calendar.WeekNumberTest:.
    com.android.calendar.widget.CalendarAppWidgetServiceTest:..
    Test results for InstrumentationTestRunner=......................
    Time: 0.165
    
    OK (22 tests)
    
    BUG: 189873985
    Change-Id: Ib8e854d1aac1b0cc7d853d3467db9431f635d66b

--- src/com/android/calendar/UpgradeReceiver.kt
+++ src/com/android/calendar/UpgradeReceiver.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 The Android Open Source Project
+ * Copyright (C) 2021 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,17 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.android.calendar
 
-package com.android.calendar;
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-
-public class UpgradeReceiver extends BroadcastReceiver {
-    @Override
-    public void onReceive(final Context context, final Intent intent) {
-        Utils.trySyncAndDisableUpgradeReceiver(context);
+class UpgradeReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Utils.trySyncAndDisableUpgradeReceiver(context)
     }
-
 }
