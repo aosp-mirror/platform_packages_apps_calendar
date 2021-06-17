b813f3a6db1132f2ce5b144db7b30651905ce741
Damian Patel <damianpatel@google.com>
2021-06-17 15:20:45 +0000

    AOSP/Calendar - EventInfoActivity fully converted with bp file
    Using Android Studio, EventInfoActivty was converted
    to Kotlin. Intially the converter would set
    certain primitive types, such as long types,
    to be nullable in Kotlin (Long?) when in realilty
    this just caused various compile errors. This was
    corrected by changing the Kotlin types to only
    be nullable when necessary and through casting.
    Another issue had to do with nullable object
    references invoking functions without null
    safety. This also had to be manually implemented.
    
    Test: manual - build tests, unit tests, and
        CTS tests were conducted using the following
        commands:
        $ make Calendar
        $ make CalendarTests -j
        $ adb install -r -t -d out/target/product/bonito/
        testcases/CalendarTests/arm64/CalendarTests.apk
                Success
    
        $ adb shell am instrument -w com.android.calendar.tests
    	com.android.calendar.FormatDateRangeTest:.
    	com.android.calendar.UtilsTests:..........
    	com.android.calendar.WeekNumberTest:.
    	com.android.calendar.widget.CalendarAppWidgetServiceTest:..
    	Test results for InstrumentationTestRunner=........
    	Time: 0.192
    
    	OK (22 tests)
    
         General CTS Tests:
        $ tools/cts-tradefed
        cts-tf > run cts -m CtsProviderTestCases
    
    ================= Results ==================
    =============== Consumed Time ==============
        arm64-v8a CtsProviderTestCases: 8m 7s
    Total aggregated tests run time: 8m 7s
    ============== TOP 1 Slow Modules ==============
        arm64-v8a CtsProviderTestCases: 0.77 tests/sec [376 tests / 487482 msec]
    ============== Modules Preparation Times ==============
        arm64-v8a CtsProviderTestCases => prep = 8975 ms || clean = 1763 ms
    Total preparation time: 8s  ||  Total tear down time: 1s
    =======================================================
    =============== Summary ===============
    Total Run time: 9m 45s
    1/1 modules completed
    Total Tests       : 376
    PASSED            : 362
    FAILED            : 0
    IGNORED           : 6
    ASSUMPTION_FAILURE: 8
    ============== End of Results ==============
    
    Change-Id: Iaeac5c6511ee8cd50b83f4c7d3819fbfbb2cff51

--- src/com/android/calendar/EventInfoActivity.kt
+++ src/com/android/calendar/EventInfoActivity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 The Android Open Source Project
+ * Copyright (C) 2021 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,178 +13,184 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.calendar;
+package com.android.calendar
 
-import static android.provider.CalendarContract.EXTRA_EVENT_BEGIN_TIME;
-import static android.provider.CalendarContract.EXTRA_EVENT_END_TIME;
-import static android.provider.CalendarContract.Attendees.ATTENDEE_STATUS;
+import android.provider.CalendarContract.EXTRA_EVENT_BEGIN_TIME
+import android.provider.CalendarContract.EXTRA_EVENT_END_TIME
+import android.provider.CalendarContract.Attendees.ATTENDEE_STATUS
+import android.app.ActionBar
+import android.app.Activity
+import android.app.FragmentManager
+import android.app.FragmentTransaction
+import android.content.Intent
+import android.content.res.Resources
+import android.database.ContentObserver
+import android.net.Uri
+import android.os.Bundle
+import android.os.Handler
+import android.provider.CalendarContract
+import android.provider.CalendarContract.Attendees
+import android.util.Log
+import android.widget.Toast
 
-import android.app.ActionBar;
-import android.app.Activity;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.Intent;
-import android.content.res.Resources;
-import android.database.ContentObserver;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Handler;
-import android.provider.CalendarContract;
-import android.provider.CalendarContract.Attendees;
-import android.util.Log;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class EventInfoActivity extends Activity {
-    private static final String TAG = "EventInfoActivity";
-    private EventInfoFragment mInfoFragment;
-    private long mStartMillis, mEndMillis;
-    private long mEventId;
+class EventInfoActivity : Activity() {
+    private var mInfoFragment: EventInfoFragment? = null
+    private var mStartMillis: Long = 0
+    private var mEndMillis: Long = 0
+    private var mEventId: Long = 0
 
     // Create an observer so that we can update the views whenever a
     // Calendar event changes.
-    private final ContentObserver mObserver = new ContentObserver(new Handler()) {
+    private val mObserver: ContentObserver = object : ContentObserver(Handler()) {
         @Override
-        public boolean deliverSelfNotifications() {
-            return false;
+        override fun deliverSelfNotifications(): Boolean {
+            return false
         }
 
         @Override
-        public void onChange(boolean selfChange) {
-            if (selfChange) return;
-            if (mInfoFragment != null) {
-                mInfoFragment.reloadEvents();
+        override fun onChange(selfChange: Boolean) {
+            if (selfChange) return
+            val temp = mInfoFragment
+            if (temp != null) {
+                mInfoFragment?.reloadEvents()
             }
-        }
-    };
-
-    @Override
-    protected void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-
-        // Get the info needed for the fragment
-        Intent intent = getIntent();
-        int attendeeResponse = 0;
-        mEventId = -1;
-        boolean isDialog = false;
-
-        if (icicle != null) {
-            mEventId = icicle.getLong(EventInfoFragment.BUNDLE_KEY_EVENT_ID);
-            mStartMillis = icicle.getLong(EventInfoFragment.BUNDLE_KEY_START_MILLIS);
-            mEndMillis = icicle.getLong(EventInfoFragment.BUNDLE_KEY_END_MILLIS);
-            attendeeResponse = icicle.getInt(EventInfoFragment.BUNDLE_KEY_ATTENDEE_RESPONSE);
-            isDialog = icicle.getBoolean(EventInfoFragment.BUNDLE_KEY_IS_DIALOG);
-        } else if (intent != null && Intent.ACTION_VIEW.equals(intent.getAction())) {
-            mStartMillis = intent.getLongExtra(EXTRA_EVENT_BEGIN_TIME, 0);
-            mEndMillis = intent.getLongExtra(EXTRA_EVENT_END_TIME, 0);
-            attendeeResponse = intent.getIntExtra(ATTENDEE_STATUS,
-                    Attendees.ATTENDEE_STATUS_NONE);
-            Uri data = intent.getData();
-            if (data != null) {
-                try {
-                    List<String> pathSegments = data.getPathSegments();
-                    int size = pathSegments.size();
-                    if (size > 2 && "EventTime".equals(pathSegments.get(2))) {
-                        // Support non-standard VIEW intent format:
-                        //dat = content://com.android.calendar/events/[id]/EventTime/[start]/[end]
-                        mEventId = Long.parseLong(pathSegments.get(1));
-                        if (size > 4) {
-                            mStartMillis = Long.parseLong(pathSegments.get(3));
-                            mEndMillis = Long.parseLong(pathSegments.get(4));
-                        }
-                    } else {
-                        mEventId = Long.parseLong(data.getLastPathSegment());
-                    }
-                } catch (NumberFormatException e) {
-                    if (mEventId == -1) {
-                        // do nothing here , deal with it later
-                    } else if (mStartMillis == 0 || mEndMillis ==0) {
-                        // Parsing failed on the start or end time , make sure the times were not
-                        // pulled from the intent's extras and reset them.
-                        mStartMillis = 0;
-                        mEndMillis = 0;
-                    }
-                }
-            }
-        }
-
-        if (mEventId == -1) {
-            Log.w(TAG, "No event id");
-            Toast.makeText(this, R.string.event_not_found, Toast.LENGTH_SHORT).show();
-            finish();
-        }
-
-        // If we do not support showing full screen event info in this configuration,
-        // close the activity and show the event in AllInOne.
-        Resources res = getResources();
-        if (!res.getBoolean(R.bool.agenda_show_event_info_full_screen)
-                && !res.getBoolean(R.bool.show_event_info_full_screen)) {
-            CalendarController.getInstance(this)
-                    .launchViewEvent(mEventId, mStartMillis, mEndMillis, attendeeResponse);
-            finish();
-            return;
-        }
-
-        setContentView(R.layout.simple_frame_layout);
-
-        // Get the fragment if exists
-        mInfoFragment = (EventInfoFragment)
-                getFragmentManager().findFragmentById(R.id.main_frame);
-
-
-        // Remove the application title
-        ActionBar bar = getActionBar();
-        if (bar != null) {
-            bar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP | ActionBar.DISPLAY_SHOW_HOME);
-        }
-
-        // Create a new fragment if none exists
-        if (mInfoFragment == null) {
-            FragmentManager fragmentManager = getFragmentManager();
-            FragmentTransaction ft = fragmentManager.beginTransaction();
-            mInfoFragment = new EventInfoFragment(this, mEventId, mStartMillis, mEndMillis,
-                    attendeeResponse, isDialog, (isDialog ?
-                            EventInfoFragment.DIALOG_WINDOW_STYLE :
-                                EventInfoFragment.FULL_WINDOW_STYLE));
-            ft.replace(R.id.main_frame, mInfoFragment);
-            ft.commit();
         }
     }
 
     @Override
-    protected void onNewIntent(Intent intent) {
+    protected override fun onCreate(icicle: Bundle?) {
+        super.onCreate(icicle)
+
+        // Get the info needed for the fragment
+        val intent: Intent = getIntent()
+        var attendeeResponse = 0
+        mEventId = -1
+        var isDialog = false
+        if (icicle != null) {
+            mEventId = icicle.getLong(EventInfoFragment.BUNDLE_KEY_EVENT_ID)
+            mStartMillis = icicle.getLong(EventInfoFragment.BUNDLE_KEY_START_MILLIS)
+            mEndMillis = icicle.getLong(EventInfoFragment.BUNDLE_KEY_END_MILLIS)
+            attendeeResponse = icicle.getInt(EventInfoFragment.BUNDLE_KEY_ATTENDEE_RESPONSE)
+            isDialog = icicle.getBoolean(EventInfoFragment.BUNDLE_KEY_IS_DIALOG)
+        } else if (intent != null && Intent.ACTION_VIEW.equals(intent.getAction())) {
+            mStartMillis = intent.getLongExtra(EXTRA_EVENT_BEGIN_TIME, 0)
+            mEndMillis = intent.getLongExtra(EXTRA_EVENT_END_TIME, 0)
+            attendeeResponse = intent.getIntExtra(
+                ATTENDEE_STATUS,
+                Attendees.ATTENDEE_STATUS_NONE
+            )
+            val data: Uri? = intent.getData()
+            if (data != null) {
+                try {
+                    val pathSegments = data.getPathSegments()
+                    val size: Int = pathSegments.size
+                    if (size > 2 && "EventTime".equals(pathSegments[2])) {
+                        // Support non-standard VIEW intent format:
+                        // dat = content://com.android.calendar/events/[id]/EventTime/[start]/[end]
+                        mEventId = pathSegments[1].toLong()
+                        if (size > 4) {
+                            mStartMillis = pathSegments[3].toLong()
+                            mEndMillis = pathSegments[4].toLong()
+                        }
+                    } else {
+                        mEventId = data.getLastPathSegment() as Long
+                    }
+                } catch (e: NumberFormatException) {
+                    if (mEventId == -1L) {
+                        // do nothing here , deal with it later
+                    } else if (mStartMillis == 0L || mEndMillis == 0L) {
+                        // Parsing failed on the start or end time , make sure the times were not
+                        // pulled from the intent's extras and reset them.
+                        mStartMillis = 0
+                        mEndMillis = 0
+                    }
+                }
+            }
+        }
+        if (mEventId == -1L) {
+            Log.w(TAG, "No event id")
+            Toast.makeText(this, R.string.event_not_found, Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+        // If we do not support showing full screen event info in this configuration,
+        // close the activity and show the event in AllInOne.
+        val res: Resources = getResources()
+        if (!res.getBoolean(R.bool.agenda_show_event_info_full_screen) &&
+            !res.getBoolean(R.bool.show_event_info_full_screen)
+        ) {
+            CalendarController.getInstance(this)
+                .launchViewEvent(mEventId, mStartMillis, mEndMillis, attendeeResponse)
+            finish()
+            return
+        }
+        setContentView(R.layout.simple_frame_layout)
+
+        // Get the fragment if exists
+        mInfoFragment = getFragmentManager().findFragmentById(R.id.main_frame) as EventInfoFragment
+
+        // Remove the application title
+        val bar: ActionBar? = getActionBar()
+        if (bar != null) {
+            bar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP or ActionBar.DISPLAY_SHOW_HOME)
+        }
+
+        // Create a new fragment if none exists
+        if (mInfoFragment == null) {
+            val fragmentManager: FragmentManager = getFragmentManager()
+            val ft: FragmentTransaction = fragmentManager.beginTransaction()
+            mInfoFragment = EventInfoFragment(
+                this,
+                mEventId,
+                mStartMillis,
+                mEndMillis,
+                attendeeResponse,
+                isDialog,
+                if (isDialog) EventInfoFragment.DIALOG_WINDOW_STYLE
+                    else EventInfoFragment.FULL_WINDOW_STYLE
+            )
+            ft.replace(R.id.main_frame, mInfoFragment)
+            ft.commit()
+        }
+    }
+
+    @Override
+    protected override fun onNewIntent(intent: Intent?) {
         // From the Android Dev Guide: "It's important to note that when
         // onNewIntent(Intent) is called, the Activity has not been restarted,
         // so the getIntent() method will still return the Intent that was first
         // received with onCreate(). This is why setIntent(Intent) is called
         // inside onNewIntent(Intent) (just in case you call getIntent() at a
         // later time)."
-        setIntent(intent);
-    }
-
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
+        setIntent(intent)
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        getContentResolver().registerContentObserver(CalendarContract.Events.CONTENT_URI,
-                true, mObserver);
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        getContentResolver().unregisterContentObserver(mObserver);
+    protected override fun onResume() {
+        super.onResume()
+        getContentResolver().registerContentObserver(
+            CalendarContract.Events.CONTENT_URI,
+            true, mObserver
+        )
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    protected override fun onPause() {
+        super.onPause()
+        getContentResolver().unregisterContentObserver(mObserver)
+    }
+
+    @Override
+    protected override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    companion object {
+        private const val TAG = "EventInfoActivity"
     }
 }
